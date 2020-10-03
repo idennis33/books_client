51ece0fa29eac2688b909e8026a6817f6b9a4af9
Isaac Dennis <idennisgsu3@gmail.com>
2020-10-02 21:44:17 -0400

    went back to add img to edit

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -82,6 +82,7 @@ background-color: grey;
 						onChange={handleChange}
 						value={book.author}
 					/>
+					<img src={img}></img><br/>
                     Description:{' '}
 					<input
 						type="text"
