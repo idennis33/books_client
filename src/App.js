6f4998994531f7a560a679234c1ac24d5e6edab4
Isaac Dennis <idennisgsu3@gmail.com>
2020-10-02 21:08:55 -0400

    still trying get code to work

--- src/App.js
+++ src/App.js
@@ -95,7 +95,7 @@ export default function App() {
   };
   const getBooks = async () => {
     try {
-      const response = await fetch("https://bookshelves-app-api.herokuapp.com");
+      const response = await fetch("https://bookshelves-app-api.herokuapp.com/books");
       const data = await response.json();
       setBooks(data);
     } catch (error) {
@@ -113,7 +113,7 @@ export default function App() {
     event.preventDefault();
     try {
       const response = await axios.post(
-        "https://bookshelves-app-api.herokuapp.com",
+        "https://bookshelves-app-api.herokuapp.com/books",
         formInputs
       );
       const data = response.data;
