fe34e40c6d1dfdd847448582ef8586c6a7f3dd64
Isaac Dennis <idennisgsu3@gmail.com>
2020-10-02 20:22:37 -0400

    added books to url for heroku

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ export default function App() {
   const handleSignUp = async (event) => {
     event.preventDefault();
     try {
-      const response = await axios.post("https://bookshelves-app-api.herokuapp.com", {
+      const response = await axios.post("https://bookshelves-app-api.herokuapp.com/books", {
         email: state.email,
         password: state.password,
       });
@@ -83,7 +83,7 @@ export default function App() {
   const handleLogIn = async (event) => {
     event.preventDefault();
     try {
-      const response = await axios.post("https://bookshelves-app-api.herokuapp.com", {
+      const response = await axios.post("https://bookshelves-app-api.herokuapp.com/books", {
         email: state.email,
         password: state.password,
       });
@@ -95,7 +95,7 @@ export default function App() {
   };
   const getBooks = async () => {
     try {
-      const response = await fetch("https://bookshelves-app-api.herokuapp.com");
+      const response = await fetch("https://bookshelves-app-api.herokuapp.com/books");
       const data = await response.json();
       setBooks(data);
     } catch (error) {
@@ -113,7 +113,7 @@ export default function App() {
     event.preventDefault();
     try {
       const response = await axios.post(
-        "https://bookshelves-app-api.herokuapp.com",
+        "https://bookshelves-app-api.herokuapp.com/books",
         formInputs
       );
       const data = response.data;
